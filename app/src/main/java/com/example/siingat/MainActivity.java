722b124384f56d9b8e2f926ebcf86e0f1749d27d
Brahma Aditama <aditama.brahma@gmail.com>
2022-03-24 19:17:35 +0700

    TopBar

--- app/src/main/java/com/example/siingat/MainActivity.java
+++ app/src/main/java/com/example/siingat/MainActivity.java
@@ -6,15 +6,29 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = findViewById(R.id.date);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String formattedDate = DateFormat.getDateInstance(DateFormat.LONG).format(currentTime);
+
+        date.setText(formattedDate);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 

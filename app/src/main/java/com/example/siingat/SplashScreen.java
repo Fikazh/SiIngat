53084a68d0ad636efa89f393c7dda8b6eb5c68ee
Brahma Aditama <aditama.brahma@gmail.com>
2022-03-28 03:21:59 +0700

    Finalisasi Splash Screen (SS)

--- app/src/main/java/com/example/siingat/SplashScreen.java
+++ app/src/main/java/com/example/siingat/SplashScreen.java
@@ -2,15 +2,21 @@ package com.example.siingat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    Animation topAnim;
+    Animation topAnim, leftAnim, rightAnim, fadeInAnim;
     TextView Welcome;
+    ImageView logo, circleShape;
+    View leftLine, rightLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,30 @@ public class SplashScreen extends AppCompatActivity {
 
         //Animation
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        leftAnim = AnimationUtils.loadAnimation(this, R.anim.left_animation);
+        rightAnim = AnimationUtils.loadAnimation(this, R.anim.right_animation);
+        fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fadein_animation);
 
         //Hooks
         Welcome = findViewById(R.id.welcome);
+        leftLine = findViewById(R.id.line_left);
+        rightLine = findViewById(R.id.line_right);
+        circleShape = findViewById(R.id.circle_shape);
+        logo = findViewById(R.id.splash_logo);
 
         Welcome.setAnimation(topAnim);
+        leftLine.setAnimation(leftAnim);
+        rightLine.setAnimation(rightAnim);
+        circleShape.setAnimation(fadeInAnim);
+        logo.setAnimation(fadeInAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1500);
     }
 }

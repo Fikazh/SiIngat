65fcc40f68ed057546876d6ef1900b77ede507ea
fikri azhari <fikriazhari551@gmail.com>
2022-03-27 17:11:52 +0700

    gabung calendar + rename file

--- app/src/main/java/com/example/siingat/CalendarDetailFragment.java
+++ app/src/main/java/com/example/siingat/CalendarDetailFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link kalender_detail#newInstance} factory method to
+ * Use the {@link CalendarDetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class kalender_detail extends Fragment {
+public class CalendarDetailFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class kalender_detail extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public kalender_detail() {
+    public CalendarDetailFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class kalender_detail extends Fragment {
      * @return A new instance of fragment kalender_detail.
      */
     // TODO: Rename and change types and number of parameters
-    public static kalender_detail newInstance(String param1, String param2) {
-        kalender_detail fragment = new kalender_detail();
+    public static CalendarDetailFragment newInstance(String param1, String param2) {
+        CalendarDetailFragment fragment = new CalendarDetailFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class kalender_detail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kalender_detail, container, false);
+        return inflater.inflate(R.layout.fragment_calendar_detail, container, false);
     }
 }
